d2232b8ef4859b5320eb2af86d85bbe8c2bc5056
Alannah Gregory <techstuff966@gmail.com>
2024-08-03 09:34:31 -0400

    Initial commit

---
+++ src/test/java/com/lana/SpringSample/SpringSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lana.SpringSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
